2ed7d3afa13023cd26fbf3417d559336769330ff
miki1981-max <miki43154@gmail.com>
2025-05-22 17:26:21 +0200

    creating files

---
+++ CareService/FireBaseCode.swift
@@ -0,0 +1,53 @@
+//
+//  FireBaseCode.swift
+//  CareService
+//
+//  Created by Kamila Ponomarova on 2025-05-22.
+//
+
+//
+//  Fire base code.swift
+//  You and me project
+//
+//  Created by Kamila Ponomarova on 2025-01-25.
+//
+
+import Foundation
+import FirebaseAuth
+
+@Observable class Firebasecode {
+    
+    var loginerror : String?
+    
+    func userRegister(email : String, password : String, name: String) {
+            Task {
+                do {
+                    let regResult = try await Auth.auth().createUser(withEmail: email, password: password)
+                    
+                    
+                } catch {
+                    print("FEL REG")
+                    loginerror = "Error reg"
+                }
+            }
+        }
+    
+    func userLogin(email : String, password : String) {
+            Task {
+                do {
+                    try await Auth.auth().signIn(withEmail: email, password: password)
+                } catch {
+                    print("FEL LOGIN")
+                    loginerror = "Error login"
+                }
+            }
+        }
+    
+    func userLogout() {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+        
+        }
+    }
+}
